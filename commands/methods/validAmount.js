219ff633c9f563f60e59e017a30002fbd9e5f0ac
MSchotsman <38855941+MSchotsman@users.noreply.github.com>
2021-03-02 21:15:31 +0100

    Coinflip with bet function
    Coinfip now has a bet functionality.
    Also as a small change validAmount is an auxiliary function that checks whether a bet amount is legit and responds accorrdingly.

---
+++ commands/methods/validAmount.js
@@ -0,0 +1,15 @@
+// Auxiliary function to determine whether a bet is actually valid
+// When it's invalid, type a message in the channel letting the user know and return false
+// When it's valid, return true
+module.exports = function (score, bet, msg) {
+    if (bet <= 0) {
+        msg.channel.send("Please bet at least 1 point.");
+        return false
+    }else if (!Number.isSafeInteger(bet)) {
+        msg.channel.send("Please bet an integer amount of points.");
+        return false
+    } else if (bet > score.points) {
+        msg.channel.send("Your bet is higher than your current total points.");
+        return true
+    }
+};
