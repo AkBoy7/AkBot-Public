89e1b5dd65d9cf7a6ec232a376cffb1f2d49fef1
MSchotsman <38855941+MSchotsman@users.noreply.github.com>
2021-03-05 15:06:37 +0100

    v5, now the fetched messages first get stored and then used

--- commands/test.js
+++ commands/test.js
@@ -1,23 +1,24 @@
 module.exports = async function(msg, args) {
-    const client = msg.client;
     const filter = m => m.author.id === msg.author.id;
-    msg.channel.awaitMessages(filter, {
+    let collected = await msg.channel.awaitMessages(filter, {
         max: 1,
         time: 10000,
         errors: ['time']
-    }).then( m => {
-        msg.channel.send("new author id: " + m.author.id);
-        msg.channel.send("old author id: " + msg.author.id);
-        msg.channel.send("Just received the following message: " + m.first().content);
-    }).catch( m => {
+    }).catch(() => {
         msg.channel.send("User did not type a message in 10 seconds");
     });
+    if (!collected) {
+        msg.channel.send("Message did not get fetched");
+    } else {
+        msg.channel.send("Fetched message contained: " + collected.first().content);
+    }
 
-    msg.channel.send("Hallelujah (of hoe je dat ook spelt)").then(message => {
-        setTimeout(() => {
-            message.edit("This text has now been edited, cool right?")
-        }, 3000);
-    })
+    // Editing works
+    // msg.channel.send("Hallelujah (of hoe je dat ook spelt)").then(message => {
+    //     setTimeout(() => {
+    //         message.edit("This text has now been edited, cool right?")
+    //     }, 3000);
+    // })
 
     msg.channel.send("That's all folks");
 
