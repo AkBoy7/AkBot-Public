7c49b6b40867d47314b3e27196230b269dde627b
MSchotsman <38855941+MSchotsman@users.noreply.github.com>
2021-03-05 14:14:13 +0100

    v3

--- commands/test.js
+++ commands/test.js
@@ -3,11 +3,14 @@ module.exports = function(msg, args) {
     const filter = m => m.author.id === msg.author.id;
     msg.channel.awaitMessages(filter, {
         max: 1,
-        time: 10000,
-        errors: ['time']
+        time: 10000
     }).then( m => {
-        msg.channel.send("Just received the following message: " + m[0].content);
+        msg.channel.send("new author id: " + m.author.id);
+        msg.channel.send("old author id: " + msg.author.id);
+        msg.channel.send("Just received the following message: " + m.first().content);
     }).catch( m => {
+        msg.channel.send("new author id: " + m.author.id);
+        msg.channel.send("old author id: " + msg.author.id);
         msg.channel.send("User did not type a message in 10 seconds");
     });
 
