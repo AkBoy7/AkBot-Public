fb2cc5ff1fbd32d037db08ffd28e892c09c15b04
Akam <akambilbas@gmail.com>
2023-02-14 11:06:34 +0100

    comment back

--- commands.js
+++ commands.js
@@ -42,10 +42,10 @@ module.exports = async function (msg) {
     userData.chatMessageCount += 1;
     client.setData.run(userData);
 
-    //for testing in specific channel
-    // if (msg.channel.id != process.env.TEST_CHANNELID) {
-    //     return;
-    // }
+    // for testing in specific channel
+    if (msg.channel.id != process.env.TEST_CHANNELID) {
+        return;
+    }
 
     //Deletes pins created message when a message of Akbot is pinned
     if(msg.type === "PINS_ADD" && msg.author.bot) {
