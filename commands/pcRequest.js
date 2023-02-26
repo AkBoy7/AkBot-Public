f8c277656d10b970347273586f7ad29850c4480b
Akam <akambilbas@gmail.com>
2023-02-26 12:08:57 +0100

    Date is now checked if it is during training day and !pc schedule implemented

--- commands/pcRequest.js
+++ commands/pcRequest.js
@@ -1,6 +1,7 @@
 const checkRole = require("./methods/checkRole.js");
 require('dotenv').config();
 let dateObj;
+let client;
 
 // Main method that is called after !pc
 module.exports = function (msg, args) {
@@ -10,6 +11,7 @@ module.exports = function (msg, args) {
         "If you do not have acces to this channel then message the board or a moderator.");
         return;
     }
+    client = msg.client;
 
     // Command: !pc 
     // This will send a help menu
@@ -26,7 +28,32 @@ module.exports = function (msg, args) {
     // Command: !pc schedule 
     // This will send a schedule of this month and next month
     if (args.length == 1 && args[0].toLowerCase() === "schedule") {
-        // TODO: send a schedule of the current and next month in an embedded message
+        let formattedSchedule = "";
+        let allDates = client.getSchedule.all();
+        var now = new Date();
+        allDates.forEach(entry => {
+            const dateObject = new Date(entry.trainingDate);
+            if (now.getMonth() >= dateObject.getMonth() && now.getMonth() + 2 < dateObject.getMonth()) {
+                const humanDate = dateObject.toLocaleString();
+                let slot1;
+                let slot2;
+                if (entry.slot1 === "") {
+                    slot1 = "---";
+                } else {
+                    slot1 = entry.slot1;
+                }
+    
+                if (entry.slot2 === "") {
+                    slot2 = "---";
+                } else {
+                    slot2 = entry.slot2;
+                }
+                formattedSchedule.push("```" + humanDate + ": Slot 1: " + slot1 + "; " + + ": Slot 2: " + slot2 + "```");
+            }
+        });
+
+        msg.channel.send(formattedSchedule);
+        return;
     }
 
     // Command: !pc request DD-MM 
@@ -83,8 +110,10 @@ function removeSlot(msg) {
     // TODO: write function
 }
 
+
 // Checks if date is actually in a date format of DD-MM and return false if not correct
 function checkDateFormat(date) {
+    // Check formatting of the date in the message
     if (!(date.content).includes("-")) {
         msg.channel.send("Wrong date format! Make sure to write it in this formate `DD-MM`. Example 31-01 for 31st of January.");
         return false;
@@ -96,13 +125,31 @@ function checkDateFormat(date) {
         return false;
     }
 
+    if (!Number.isSafeInteger(parseInt(dateParts[0], 10)) || !Number.isSafeInteger(parseInt(dateParts[1], 10))) {
+        msg.channel.send("The date requested was not a proper date format with numbers");
+        return false;
+    }
+
+    // Make a date object for later
     dateObj = new Date(parseInt(dateParts[1], 10) - 1, parseInt(dateParts[0], 10));
     if (!dateObj) {
         msg.channel.send("An unexpected error occured with processing the date.");
         return false;
     }
 
-    // TODO: check if requested date is a training day
+    // Check if requested date is a training day
+    let allDates = client.getSchedule.all();
+    let isTrainingDay = false;
+    allDates.forEach(entryDate => {
+        if (entryDate.trainingDate === date) {
+            isTrainingDay = true;
+        }
+    });
+
+    if (!isTrainingDay) {
+        msg.channel.send("The requested date is not a valid training day.");
+        return false;
+    }
 
     return true;
 }
